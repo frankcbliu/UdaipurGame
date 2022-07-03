97af2504867c59dae1e66da56b96522cc1e87f89
frankcbliu <q641259875@outlook.com>
2022-07-03 18:43:01 +0800

    fix: 修改config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export const GAME_NAME = "udaipur";
+export const GAME_SERVER_IP = "192.168.1.107"; // 使用你的内网IP
 export const GAME_SERVER_PORT = 8000;
-export const GAME_SERVER_URL = `http://192.168.1.107:${GAME_SERVER_PORT}`;
-export const WEB_SERVER_URL = "http://192.168.1.107:3000";
+export const GAME_SERVER_URL = `http://${GAME_SERVER_IP}:${GAME_SERVER_PORT}`;
+export const WEB_SERVER_URL = `http://${GAME_SERVER_IP}:3000`;
 export const APP_PRODUCTION = true;
